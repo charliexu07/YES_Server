17328a2c1f7a7d43f9e5230a889fdc0fb32cd9e2
Jiachen Xu <xujiachen1999@outlook.com>
2020-07-25 10:59:02 -0500

    gradle change

--- src/main/java/com/example/YESserver/UserRepository.java
+++ src/main/java/com/example/YESserver/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.YESserver;
 
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.example.YESserver.models.User;
 
@@ -10,7 +12,9 @@ import java.util.Optional;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findAll();
     Optional<User> findByUserName(String userName);
 }
+
+
